2fb93445a798db86cb904898cee45969b8cbad29
Abdirizackmata <abdirizackissack2018@gmail.com>
2024-04-25 05:49:32 -0700

    added some new features

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "Falde-foundation",
   plugins: [react()],
   build: {
     rollupOptions: {
